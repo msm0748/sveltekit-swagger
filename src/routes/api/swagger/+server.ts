6b86628b6d7fa05c55e73c86cb2198e2b3aba4b4
seokmin <msm0748@gmail.com>
2024-07-26 14:43:51 +0900

    remove: 필요없는 파일 삭제

--- src/routes/api/swagger/+server.ts
+++
@@ -1,7 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from '../swagger/$types';
-import { swaggerSpec } from '$lib/swagger';
-
-export const GET: RequestHandler = async () => {
-	return json(swaggerSpec);
-};
